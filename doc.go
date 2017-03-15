4c309179232a9db586229ea1af2e0f4d39603a70
Ahmet Alp Balkan <ahmetb@google.com>
2017-03-15 16:33:10 -0700

    Add godoc file pointing to samples in root package
    Context: https://groups.google.com/forum/#!topic/kubernetes-users/_HnGSuPmDoc
    
    I do not know if making the package root an actual go package by
    adding a doc.go file would have bad implications. I am not sure about the
    package name either, but I don't assume it matters.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Package clientgo contains a collection of subpackages for Go client for
+// Kubernetes.
+//
+// Examples
+//
+// See https://github.com/kubernetes/client-go/tree/master/examples for sample
+// programs written using client-go.
+package clientgo
